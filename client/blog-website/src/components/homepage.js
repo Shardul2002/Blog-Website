cc60664c6c207853f46d4f21820ce4edaf94a17d
Shardul Joshi <sharduljoshi9@gmail.com>
2023-04-29 01:12:51 +0530

    Homepage changes

--- client/blog-website/src/components/homepage.js
+++ client/blog-website/src/components/homepage.js
@@ -6,9 +6,9 @@ export default function HomePage(){
     return (
         <div>
             <div className="content">
-            <img src="/images/bkg-2.png" alt="homepage image" className="homepage-image"></img>
+            <img src="/images/unsplash.jpg" alt="homepage image" className="homepage-image"></img>
             <h1 className="carousel-caption text-muted text">Express Yourself!!</h1>
-            <p className="carousel-caption text-muted font-italic text1">A Place for your thought,fears,finds,fads,obbsession and much more....</p>
+            <p className="carousel-caption text-muted font-italic text1">A place for your thoughts,fears,finds,fads,obbsession and much more....</p>
             </div>
 
         </div>
